e6837ca28ef49b934e0a82169c7717c464937208
TabrizHabiyev <tabrizfh@code.edu.az>
2022-01-02 11:19:15 -0800

    express-handlebars configured

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 3000
+const exphbs  = require('express-handlebars')
 
+
+const port = 3000
 
 app.use(express.static('www'))
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars');
+
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname,'site/index.html'))
+  res.render('site/index')
 })
+
+app.get('/about', (req, res) => {
+  res.render('site/about')
+})
+
+app.get('/blog', (req, res) => {
+  res.render('site/blog')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('site/contact')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
